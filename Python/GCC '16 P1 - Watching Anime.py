3d5afb181d096dd198b7fd1c14eed6b4f4c83615
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-09-16 23:18:16 -0400

    brainstorming for GCC '16 P1 - Watching Anime

---
+++ Python/GCC '16 P1 - Watching Anime.py
@@ -0,0 +1,15 @@
+# BRAINSTORMING
+# https://dmoj.ca/problem/gcc16p1
+
+# init = input().split("")
+# N = range(int(init[1])); A = init[2]; C = init[3]
+#
+# e.g. 10 3 2
+# init would = [10, 3, 2]
+# N would = [1,2,3,4,5,6,7,8,9,10]
+# A would = 3
+# C would = 2
+
+# from this point on, loops can be used and remove ai-ab & ci-di from N, inclusive.
+# then finally print() the number of elements in N.
+
