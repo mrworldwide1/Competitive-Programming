fab18c94d11945f8f1605976456d03699f0bc93b
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-09-16 22:18:26 -0400

    e

--- Python/CCC '11 S1 - English or French.py
+++ Python/CCC '11 S1 - English or French.py
@@ -1,8 +1,12 @@
 N = int(input())
 t=0
 s=0
+text=[]
 
 for i in range(N):
+    text.append(input())
+
+for word in text:
         t += word.count("t")
         t += word.count("T")
         s += word.count("s")
