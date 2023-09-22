e3f4bc3854e2489951a5406dabeb3db2f5cc5c2c
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-09-22 17:48:44 -0400

    oij

--- Python/GCC '16 P1 - Watching Anime.py
+++ Python/GCC '16 P1 - Watching Anime.py
@@ -13,3 +13,5 @@
 # from this point on, loops can be used and remove ai-ab & ci-di from N, inclusive.
 # then finally print() the number of elements in N.
 
+initialLine = input().split(" ")
+N = range(int(initialLine[0])); A = range(int(initialLine[1])); C = range(int(initialLine[2]))
