05aa779be107df3075000c7e4c93c3f9eb3f2862
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-09-24 12:08:53 -0400

    optimize

--- Python/16 BIT SW ONLY.py
+++ Python/16 BIT SW ONLY.py
@@ -1,6 +1,6 @@
 for i in range(int(input())):
-    a = (list(map(int, input().split())))
-    if a[2] == a[1]*a[0]:
+    N = (list(map(int, input().split())))
+    if N[2] == N[1]*N[0]:
         print("POSSIBLE DOUBLE SIGMA")
     else:
         print("16 BIT S/W ONLY")
