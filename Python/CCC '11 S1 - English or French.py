27d3edf8351488eefd458f1d39a488effe7a9839
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-09-24 10:58:53 -0400

    optimization

--- Python/CCC '11 S1 - English or French.py
+++ Python/CCC '11 S1 - English or French.py
@@ -1,16 +1,16 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline())
 t=0
 s=0
 text=[]
 
 for i in range(N):
-    text.append(input())
+    text.append(input().lower())
 
 for word in text:
-        t += word.count("t")
-        t += word.count("T")
-        s += word.count("s")
-        s += word.count("S")
+    t += word.count("t")
+    s += word.count("s")
 
 if t > s:
     print("English")
