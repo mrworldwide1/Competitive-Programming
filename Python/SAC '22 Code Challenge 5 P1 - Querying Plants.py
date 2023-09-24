16dbaa768856c530966d51f9ab8ef0266db0eb9f
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-09-24 02:10:13 -0400

    hr

--- Python/SAC '22 Code Challenge 5 P1 - Querying Plants.py
+++ Python/SAC '22 Code Challenge 5 P1 - Querying Plants.py
@@ -1,16 +1,17 @@
 N = int(input())
 C = int(input())*2
-ans = ""
 
-plants = list(map(int, input().split()))
+plants = map(int, input().split())
 
-if len(plants) == 1:
-        plants[0] += C
-        ans += str(f"{plants[0]} ")
-        print(ans)
-else:
-    for i in plants:
-            plants[i-1] += C
-            ans += str(f"{plants[i-1]} ")
+ans = [x + C for x in plants]
 
-print(ans)
+# if len(plants) == 1:
+#         plants[0] += C
+#         ans += str(f"{plants[0]} ")
+#         print(ans)
+# else:
+#     for i in plants:
+#         i += C
+#         ans += i
+
+print(str(ans).lstrip('[').rstrip(']'))
