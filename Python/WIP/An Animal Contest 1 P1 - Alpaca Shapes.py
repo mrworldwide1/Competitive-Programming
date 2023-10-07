c5e8084508250bb40b485e302da5d3ee4b0047c5
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-10-07 02:15:12 -0400

    solved

---
+++ Python/WIP/An Animal Contest 1 P1 - Alpaca Shapes.py
@@ -0,0 +1,6 @@
+S, R = map(int, input().split())
+
+if S**2 > R**2 * 3.14:
+    print("SQUARE")
+else:
+    print("CIRCLE")
---
+++ Python/WIP/CCC '06 J2 - Roll the Dice.py
@@ -0,0 +1,20 @@
+m = int(input()); n = int(input())
+mNumbs =[]
+nNumbs =[]
+ways = 0
+
+for num in range(m):
+    mNumbs.append(num+1)
+
+for num in range(n):
+    nNumbs.append(num+1)
+nNumbs.reverse()
+
+while i < :
+    if mNumbs[i] + nNumbs[i] == 10:
+        ways += 1
+    else:
+        pass
+
+print("There is 1 way to get the sum 10.")
+print("There are 5 ways to get the sum 10.")
