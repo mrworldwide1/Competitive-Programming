21504aaec24124164e3d1e20146b5fc37a0080d3
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-10-09 11:47:20 -0400

    optimization

--- Python/WIP/GCC '16 P1 - Watching Anime.py
+++ Python/WIP/GCC '16 P1 - Watching Anime.py
@@ -2,18 +2,18 @@ totalHours = {}
 N, A, C = (map(int, input().split()))
 
 for hour in range(N):
-    totalHours[hour+1] = "cannot"
+    totalHours[hour+1] = "n"
 
 for streaming in range(A):
     ai, bi = (map(int, input().split()))
     while ai <= bi:
-        totalHours[ai] = "can"
+        totalHours[ai] = "y"
         ai+=1
 
 for commitment in range(C):
-    ci, di = (map(int, input().split()))
-    while ci <= di:
-        totalHours[ci] = "cannot"
-        ci+=1
+    ai, bi = (map(int, input().split()))
+    while ai <= bi:
+        totalHours[ai] = "n"
+        ai+=1
 
-print(list(totalHours.values()).count("can"))
+print(list(totalHours.values()).count("y"))
