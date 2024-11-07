bcea59dbc70a5566f362b2a50dc5737c5600470c
Lucas Leung <leungl16@mcmaster.ca>
2024-11-07 14:37:34 -0500

    remove WIPs

--- WIP/CCC '21 S1 - Crazy Fencing.py
+++
@@ -1,6 +0,0 @@
-N = (int(input()))
-heights_left_and_right = map(int, input().split()) 
-width = input().split())
-
-print(heights_left_and_right)
-print(width)
--- WIP/CCC S3 - Palindromic Poster.py
+++
@@ -1,8 +0,0 @@
-# N = rows
-# M = cols
-# R = num of N 
-#
-
-N, M, R, C = map(int, input().split())
-
-print(N, M, R, C)
